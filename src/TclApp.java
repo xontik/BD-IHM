f436ea36a1511497c9d9ba5a62ea2aba8221ee40
BORLET BENJAMIN p1502484 <benjamin.borlet@etu.univ-lyon1.fr>
2017-05-31 08:16:20 +0200

    init project intelij

---
+++ src/TclApp.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by xontik on 31/05/2017.
+ */
+public class TclApp {
+
+    public static void main (String []args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                //IHM windows here
+            }
+        });
+    }
+}
